1349feaabdb2100e49e1f14d647930e99798d060
Mariano Custiel <mcustiel@goodgamestudios.com>
2016-07-04 10:18:42 +0200

    Added message line with the command being run

--- tests/tests/acceptance/_bootstrap.php
+++ tests/tests/acceptance/_bootstrap.php
@@ -3,7 +3,9 @@ use Symfony\Component\Process\Process;
 
 // Here you can initialize variables that will be available to your tests
 
-$process = new Process('exec ' . APP_ROOT . 'bin/phiremock --port 8086 --ip 0.0.0.0');
+$command = 'exec ' . APP_ROOT . 'bin/phiremock --port 8086 --ip 0.0.0.0';
+echo 'Running ' . $command . PHP_EOL;
+$process = new Process($command);
 
 register_shutdown_function(function () use ($process) {
     $process->stop(10, SIGTERM);
