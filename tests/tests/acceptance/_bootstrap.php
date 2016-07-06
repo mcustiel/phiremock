acc0ea5b5b2257fae1012d2a52a85873cf44428d
Mariano Custiel <jmcustiel@gmail.com>
2016-07-06 23:59:17 +0200

    Prefixed with php, just to test

--- tests/tests/acceptance/_bootstrap.php
+++ tests/tests/acceptance/_bootstrap.php
@@ -3,7 +3,7 @@ use Symfony\Component\Process\Process;
 
 // Here you can initialize variables that will be available to your tests
 
-$command = 'exec ' . APP_ROOT . 'bin/phiremock --port 8086 --ip 0.0.0.0';
+$command = 'exec php ' . APP_ROOT . 'bin/phiremock --port 8086 --ip 0.0.0.0';
 echo 'Running ' . $command . PHP_EOL;
 $process = new Process($command);
 
