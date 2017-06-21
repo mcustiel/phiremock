96be6ef583adab71ec90fd68732a165829f992f0
Mariano Custiel <jmcustiel@gmail.com>
2017-06-21 23:54:26 +0200

    Fixed tests

--- tests/tests/acceptance/_bootstrap.php
+++ tests/tests/acceptance/_bootstrap.php
@@ -4,20 +4,20 @@ use Symfony\Component\Process\Process;
 
 // Here you can initialize variables that will be available to your tests
 
-// $command = 'exec php ' . APP_ROOT . 'bin/phiremock --port 8086 -d';
-// echo 'Running ' . $command . PHP_EOL;
-// $process = new Process($command);
+$command = 'exec php ' . APP_ROOT . 'bin/phiremock --port 8086';
+echo 'Running ' . $command . PHP_EOL;
+$process = new Process($command);
 
-// register_shutdown_function(function () use ($process) {
-//     echo 'Terminating phiremock' . PHP_EOL;
-//     $process->stop(10, SIGTERM);
-// });
+register_shutdown_function(function () use ($process) {
+    echo 'Terminating phiremock' . PHP_EOL;
+    $process->stop(10, SIGTERM);
+});
 
-// $process->start(function ($type, $buffer) {
-//     if (Process::ERR === $type) {
-//         echo 'ERR > ' . $buffer;
-//     } else {
-//         echo 'OUT > ' . $buffer;
-//     }
-// });
-// sleep(1);
+$process->start(function ($type, $buffer) {
+    if (Process::ERR === $type) {
+        echo 'ERR > ' . $buffer;
+    } else {
+        echo 'OUT > ' . $buffer;
+    }
+});
+sleep(1);
