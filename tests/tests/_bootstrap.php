3d5fd5046dbc16974303c1e901b044e81903905f
Denis Rudoi <Denis_Rudoi@epam.com>
2017-09-19 12:39:27 +0300

    Issue #21: List all expectations failed with annotation not found error

--- tests/tests/_bootstrap.php
+++ tests/tests/_bootstrap.php
@@ -1,7 +1,3 @@
 <?php
 
 define('APP_ROOT', __DIR__ . '/../../');
-
-$loader = require APP_ROOT . 'vendor/autoload.php';
-
-\Doctrine\Common\Annotations\AnnotationRegistry::registerLoader([$loader, 'loadClass']);
