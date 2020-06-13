fa84514d9916061acac1b1a90d18ccf71819af01
Mariano Custiel <jmcustiel@gmail.com>
2020-06-13 17:38:58 +0200

    added tests for requests listing

---
+++ tests/tests/v1/RequestListCest.php
@@ -0,0 +1,70 @@
+<?php
+
+class ExpectationListCest
+{
+    public function _before(AcceptanceTester $I)
+    {
+        $I->sendDELETE('/__phiremock/expectations');
+        $I->sendDELETE('/__phiremock/executions');
+    }
+
+    public function returnEmptyList(AcceptanceTester $I)
+    {
+        $I->sendPUT('/__phiremock/executions');
+        $I->seeResponseCodeIs('200');
+        $I->seeResponseEquals('[]');
+    }
+
+    public function returnAllExecutedRequest(AcceptanceTester $I)
+    {
+        $I->haveHttpHeader('Content-Type', 'application/json');
+        $I->sendPOST(
+            '/__phiremock/expectations',
+            [
+                'request' => [
+                    'url' => ['isEqualTo' => '/the/request/url'],
+                ],
+                'response' => [
+                    'statusCode' => 201,
+                ],
+            ]
+            );
+
+        $I->sendGET('/the/request/url');
+        $I->seeResponseCodeIs('201');
+
+        $I->sendPUT('/__phiremock/executions', '');
+        $I->seeResponseCodeIs('200');
+        $I->seeResponseEquals('[{"method":"GET","url":"http:\/\/localhost:8086\/the\/request\/url","headers":{"Host":["localhost:8086"],"User-Agent":["Symfony BrowserKit"],"Content-Type":["application\/json"],"Referer":["http:\/\/localhost:8086\/__phiremock\/expectations"]},"cookies":[],"body":""}]');
+    }
+
+    public function returnExecutedRequestMatchingExpectation(AcceptanceTester $I)
+    {
+        $I->haveHttpHeader('Content-Type', 'application/json');
+        $I->sendPOST(
+            '/__phiremock/expectations',
+            [
+                'request' => [
+                    'url' => ['isEqualTo' => '/the/request/url'],
+                ],
+                'response' => [
+                    'statusCode' => 201,
+                ],
+            ]
+        );
+
+        $I->sendGET('/the/request/url');
+        $I->seeResponseCodeIs('201');
+
+        $I->sendPUT('/__phiremock/executions', [
+            'request' => [
+                'url' => ['isEqualTo' => '/the/request/url'],
+            ],
+            'response' => [
+                'statusCode' => 201,
+            ],
+        ]);
+        $I->seeResponseCodeIs('200');
+        $I->seeResponseEquals('[{"method":"GET","url":"http:\/\/localhost:8086\/the\/request\/url","headers":{"Host":["localhost:8086"],"User-Agent":["Symfony BrowserKit"],"Content-Type":["application\/json"],"Referer":["http:\/\/localhost:8086\/__phiremock\/expectations"]},"cookies":[],"body":""}]');
+    }
+}
